6d47f7c471741c38f81f2658746568b18ee3644a
Becca Elenzil <becca@adadevelopersacademy.org>
2021-09-07 13:12:52 -0700

    coffee price

--- unit-1/fundamentals.md
+++ unit-1/fundamentals.md
@@ -1,5 +1,7 @@
 # Coffee Price
 
+**Let's write a function `calculate_coffee_price` to calculate the price of a coffee drink!**
+
 ### There are 3 different types of coffee, each with a different base price
 * Drip, $1.50
 * Latte, $3.70
@@ -9,3 +11,15 @@
 * Small, base price
 * Medium, base price X 1.3
 * Large, base price X 1.6
+
+## Vocabulary
+- function signature
+- invoke/call
+- literal
+- variable
+- statement
+- expression
+- conditional expression
+- boolean
+- truthy
+- falsy
--- unit-1/fundamentals.py
+++ unit-1/fundamentals.py
@@ -3,64 +3,124 @@ SIZES = ["small", "medium", "large"]
 TYPE_PRICES = [1.5, 3.7, 3.2]
 SIZE_PRICES = [1, 1.3, 1.6]
 
-def price_by_type(type):
-    if type not in TYPES:
-        return False
-    elif type == "drip":
+def coffee_price(type, size):
+    if type == "drip":
         price = 1.5
     elif type == "latte":
         price = 3.7
     elif type == "cappuccino":
         price = 3.2
+    # else:
+    #     return False
+
+    if size == "small":
+       price *= 1
+    elif size == "medium":
+       price *= 1.3
+    elif size == "large":
+       price *= 1.6
+    # else:
+    #     return False
+
+    return f"{price:0.2f}"
+
+price = coffee_price("drip", "medium")
+print(price)
+
+
+def price_by_type(type):
+    if type == "drip":
+        price = 1.5
+    elif type == "latte":
+        price = 3.7
+    elif type == "cappuccino":
+        price = 3.2
+    else:
+        return False
     
     return price
 
 def price_by_size(size):
-    if size not in SIZES:
-        return False
-    elif size == "small":
+    if size == "small":
         multiplier = 1
     elif size == "medium":
         multiplier = 1.3
     elif size == "large":
         multiplier = 1.6
+    else:
+        return False
 
     return multiplier
 
-def coffee_price(type, size):
 
-    if not price_by_type(type):
-        return "Invalid type"
+def coffee_price_refactor(type, size):
+    
+    # if type not in TYPES:
+    #     return "Invalid type"
 
-    if not price_by_size(size):
-        return "Invalid size"
+    # if size not in SIZES:
+    #     return "Invalid size"
     
     price = price_by_type(type)
-    price *= price_by_size(size)
+    multiplier = price_by_size(size)
 
-    return f'{price:0.2f}'
+    if not price:
+        return "Please enter a valid type."
+    
+    if not multiplier:
+        return "Please enter a valid size."
 
+    return f"{price*multiplier:0.2f}"
 
-
-def coffee_price(type, size):
-    if type not in TYPES:
-        return False
-    elif type == "drip":
-        price = 1.5
-    elif type == "latte":
-        price = 3.7
-    elif type == "cappuccino":
-        price = 3.2
-
-    if size not in SIZES:
-        return False
-    elif size == "small":
-        multiplier = 1
-    elif size == "medium":
-        multiplier = 1.3
-    elif size == "large":
-        multiplier = 1.6
-
-price = coffee_price("drip", "medium")
+price = coffee_price_refactor("drip", "medium")
 print(price)
+
+def coffee_price_loops(type, size):
+
+    for i in range(len(TYPES)):
+        if type == TYPES[i]:
+            price = TYPE_PRICES[i]
+
+    for i in range(len(SIZES)):
+        if size == SIZES[i]:
+            price *= SIZE_PRICES[i]
+
+    return f"{price:0.2f}"
+
+price = coffee_price_loops("drip", "medium")
+print(price)          
+
+
+def price_by_type(type):
+    for i in range(len(TYPES)):
+        if type == TYPES[i]:
+            price = TYPE_PRICES[i]
+    
+    return price
+
+def price_by_size(size):
+    for i in range(len(SIZES)):
+        if size == SIZES[i]:
+            multiplier = SIZE_PRICES[i]
+
+    return multiplier
+
+
+
+# def coffee_price_refactor(type, size):
+
+#     if type not in TYPES:
+#         return "Invalid type"
+
+#     if size not in SIZES:
+#         return "Invalid size"
+    
+#     price = price_by_type(type)
+#     price *= price_by_size(size)
+
+#     return f"{price:0.2f}"
+
+
+
+
 
